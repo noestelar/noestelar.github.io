e396ee38474730042a1137a0b3957662bc8eb925
Noé Rivera <tsnoe.dali@gmail.com>
2023-03-28 18:49:35 -0600

    remove tailwind custom breakpoints

--- astro-pfolio/tailwind.config.cjs
+++ astro-pfolio/tailwind.config.cjs
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			screens: {
-				sm: { min: '0px', max: '768px' },
-				md: { min: '768px', max: '1023px' },
-				lg: { min: '1024px', max: '1919px' },
-				xl: { min: '1920px' },
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+    //   screens: {
+    //     xs: "480px",
+    //     sm: "640px",
+    //     md: "768px",
+    //     lg: "1024px",
+    //     xl: "1280px",
+    //     "2xl": "1536px",
+    //   },
+      // 	sm: { min: '0px', max: '768px' },
+      // 	md: { min: '768px', max: '1023px' },
+      // 	lg: { min: '1024px', max: '1919px' },
+      // 	xl: { min: '1920px' },
+      // }
+    },
+  },
+  plugins: [],
+};
