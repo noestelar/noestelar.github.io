4d9ff7d81611ee22b6a436047a6ee5f9fc2fa917
Noé Rivera <tsnoe.dali@gmail.com>
2023-04-28 11:55:21 -0600

    add project card component

--- astro-pfolio/src/components/projects/projectCard.tsx
+++ astro-pfolio/src/components/projects/projectCard.tsx
@@ -1,23 +1,15 @@
-const RepoCard = (repo: any) => {
+const ProjectCard = ({ children, title, url, thumbnail }) => {
   return (
-    <div className="dark:bg-grayed-100 bg-grayed-50 backdrop-blur-sm rounded-lg overflow-hidden shadow-md ">
+    <div className="dark:bg-grayed-100 bg-grayed-50 backdrop-blur-sm shadow-md rounded-lg overflow-hidden  text-center">
+      <img src={thumbnail} alt={title} className="" placeholder="blur"/>
       <div className="px-6 py-5 ">
-        <div className="font-bold text-lg mb-2 text-black">
-          {repo.repo.name}
+        <div className="font-bold text-lg mb-2 dark:text-white text-grayed-100">
+          {title ? title : ""}
         </div>
-        <p className="text-white text-base ">{repo.repo.description}</p>
-      </div>
-      <div className="px-6 pt-4 pb-2">
-        <a
-          href={repo.repo.html_url}
-          target="_blank"
-          className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-semibold py-2 px-4 rounded inline-flex items-center"
-        >
-          <span>View on GitHub</span>
-        </a>
+        <p className="text-white text-base ">{children}</p>
       </div>
     </div>
   );
 };
 
-export default RepoCard;
+export default ProjectCard;
