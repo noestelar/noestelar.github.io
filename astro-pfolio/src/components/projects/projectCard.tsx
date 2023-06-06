ca1886b34ea4cf6443a983724d3ab29cad20e77c
Noé Rivera <tsnoe.dali@gmail.com>
2023-06-06 10:41:57 -0600

    misc

--- astro-pfolio/src/components/projects/projectCard.tsx
+++ astro-pfolio/src/components/projects/projectCard.tsx
@@ -1,12 +1,12 @@
 const ProjectCard = ({ children, title, url, thumbnail }) => {
   return (
-    <div className="dark:bg-grayed-100 bg-grayed-50 backdrop-blur-sm shadow-md rounded-lg overflow-hidden  text-center">
+    <div className=" dark:bg-grayed-100 bg-grayed-50 backdrop-blur-sm shadow-md rounded-lg overflow-hidden  text-center">
       <img src={thumbnail} alt={title} className="" placeholder="blur"/>
       <div className="px-6 py-5 ">
-        <div className="font-bold text-lg mb-2 dark:text-white text-grayed-100">
+        <div className="font-bold text-lg mb-2 dark:text-white text-black">
           {title ? title : ""}
         </div>
-        <p className="text-white text-base ">{children}</p>
+        <p className="dark:text-white text-base text-black ">{children}</p>
       </div>
     </div>
   );
