089db9567d0a357ecd8f22597bde3c7d6e90895b
Noé Rivera <tsnoe.dali@gmail.com>
2023-06-21 14:20:06 -0600

    justify text on timeline

--- src/components/indexPage/bioTimeLine/bioTimeLine.jsx
+++ src/components/indexPage/bioTimeLine/bioTimeLine.jsx
@@ -27,12 +27,14 @@ function BioTimeLine() {
     <section className="max-w-screen-md w-full sm:mx-0 md:px-20 px-10">
       <h1 className="text-lg font-bold underline underline-offset-2">Bio</h1>
 
-      <div className="max-w-3xl mx-auto p-4">
+      <div className="max-w-3xl mx-auto p-4 text-justify">
         <ul className="timeline">
           {events.map((event, key) => (
             <li key={key}>
               <div className="timeline-badge ">
-                <span className=" dark:text-cyaned-500 text-cyaned-700 font-bold">{event.date}</span>
+                <span className=" dark:text-cyaned-500 text-cyaned-700 font-bold">
+                  {event.date}
+                </span>
               </div>
               <div className="mt-2">
                 <div className="">
