681beee66bbdfa403da2345ed2b9b21d0faf99ba
Noé Rivera <tsnoe.dali@gmail.com>
2023-06-21 11:23:14 -0600

    Change colors for projectcard

--- astro-pfolio/src/components/projects/projectCard.tsx
+++ astro-pfolio/src/components/projects/projectCard.tsx
@@ -1,6 +1,6 @@
 const ProjectCard = ({ children, title, url, thumbnail }: any) => {
   return (
-    <div className=" dark:bg-grayed-100 bg-grayed-50 backdrop-blur-sm shadow-md rounded-lg overflow-hidden  text-center">
+    <div className=" dark:bg-grayed-100 bg-slate-100 backdrop-blur-sm shadow-md rounded-lg overflow-hidden  text-center">
       <img src={thumbnail} alt={title} className="" placeholder="blur"/>
       <div className="px-6 py-5 ">
         <div className="font-bold text-lg mb-2 dark:text-white text-black">
