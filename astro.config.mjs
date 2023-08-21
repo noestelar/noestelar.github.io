41c5d5a4cb23454e94bc0cc518802e392e5e1d77
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-08-21 14:53:20 -0600

    add styles to markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import addClasses from 'rehype-add-classes';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -11,4 +12,22 @@ export default defineConfig({
   site: 'https://noe-ali.github.io',
   // base: '/Astropfolio',
   integrations: [tailwind(), react()],
+  markdown: {
+    // extendDefaultPlugins: true,
+    rehypePlugins: [
+      [
+        addClasses,
+        {
+          h1: 'text-4xl font-quicksand font-bold',
+          h2: 'text-2xl font-quicksand font-bold',
+          h3: 'text-xl font-quicksand font-bold',
+          h4: ' font-quicksand font-bold',
+          h5: ' font-quicksand font-bold',
+          img: 'border border-slate-300 dar:border-zinc-700 rounded-xl mb-6',
+          p: 'mb-6',
+          a: 'underline underline-offset-2 hover:text-cyaned-500 decoration-cyaned-500',
+        },
+      ]
+    ]
+  }
 });
