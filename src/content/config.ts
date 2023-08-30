36eedd9ed0d4398667f8330c36bb938e499ae98f
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-08-30 16:33:03 -0600

    change collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,28 +1,28 @@
 import { defineCollection, z } from "astro:content";
 
-const projectsCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    website: z.string().optional(),
-    thumbnail: z.string(),
-    description: z.string(),
-    githubLink: z.string(),
-    platform: z.string(),
-    stack: z.string(),
-  }),
-});
-
-// const usesCollection = defineCollection({
+// const projectsCollection = defineCollection({
 //   type: "content",
 //   schema: z.object({
 //     title: z.string(),
+//     website: z.string().optional(),
 //     thumbnail: z.string(),
 //     description: z.string(),
+//     githubLink: z.string(),
+//     platform: z.string(),
+//     stack: z.string(),
 //   }),
 // });
 
+const usesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    thumbnail: z.string(),
+    description: z.string(),
+  }),
+});
+
 export const collections = {
-  projects: projectsCollection,
-  // uses: usesCollection,
+  // projects: projectsCollection,
+  uses: usesCollection,
 };
