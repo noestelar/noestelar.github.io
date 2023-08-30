c924c8aac6db9e4f024d416d2f431337b3d9e938
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-08-30 16:32:34 -0600

    add localization files

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.works": "Works",
+    "nav.uses": "Uses",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.works": "Trabajos",
+    "nav.uses": "Equipo que uso",
+  },
+} as const;
