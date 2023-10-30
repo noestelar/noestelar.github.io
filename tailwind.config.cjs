63349b7b88250e6d80ba26af71eec9d56285ebcc
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-10-29 21:28:42 -0600

    fix responsive on uses and projects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,19 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-    //   screens: {
-    //     xs: "480px",
-    //     sm: "640px",
-    //     md: "768px",
-    //     lg: "1024px",
-    //     xl: "1280px",
-    //     "2xl": "1536px",
-    //   },
-      // 	sm: { min: '0px', max: '768px' },
-      // 	md: { min: '768px', max: '1023px' },
-      // 	lg: { min: '1024px', max: '1919px' },
-      // 	xl: { min: '1920px' },
-      // }
+      // screens: {
+      // //   xs: "480px",
+      // //   sm: "640px",
+      // //   md: "768px",
+      // //   lg: "1024px",
+      // //   xl: "1280px",
+      // //   "2xl": "1536px",
+      // // },
+      // 	// sm: { min: '0px', max: '768px' },
+      // 	// md: { min: '768px' },
+      // 	// lg: { min: '1024px', max: '1919px' },
+      // 	// xl: { min: '1920px' },
+      // },
       colors:{
         cyaned: {
           50: '#e6fffa',
