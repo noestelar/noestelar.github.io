83ee90c0a85842d2cf8c639e16fb133ee2d66936
Noé Rivera Lugo <tsnoe.dali@gmail.com>
2023-10-29 21:28:22 -0600

    remove classes for responsive

--- src/components/projects/equipmentPost.tsx
+++ src/components/projects/equipmentPost.tsx
@@ -1,15 +1,14 @@
 const EquipmentPost = ({ children, title, url, thumbnail }: any) => {
   return (
     <div className="">
-      <div className="  backdrop-blur-sm shadow-md rounded-lg overflow-hidden border-2 border-gray-300 ">
-        <div className=" h-72 md:max-h-44 max-h-64  ">
-          <img
-            src={thumbnail}
-            alt={title}
-            className="object-top object-cover aspect-w-2 aspect-h-1 w-[720*2]"
-            placeholder="blur"
-          />
-        </div>
+      <div className="backdrop-blur-sm shadow-md rounded-md border-2 border-gray-300 md:max-h-40 sm:max-h-[360px] max-h-48 overflow-hidden">
+        <img
+          src={thumbnail}
+          alt={title}
+          aspectRatio={2.0}
+          className="object-top object-cover "
+          placeholder="blur"
+        />
       </div>
       <div className="px-6 py-5 text-center">
         <div className="font-bold text-lg mb-2 dark:text-white text-black">
